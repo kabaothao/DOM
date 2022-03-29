6e256c11f0be50c3015b787508e4d918e31d23ea
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-29 09:08:59 -0500

    added comments

--- DOM/Program.cs
+++ DOM/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+/*
+ What is Document Object Model?
+
+Tree of nodes/elements created by the browser
+JavaScript can be used to read/write/manipulate to the DOM
+Object Oriented Representation'
+
+
+
+ */
